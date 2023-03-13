4f512c4145b4623224d98db35875da31e236b748
Diego Ferreira <diego.nascimento91@gmail.com>
2023-03-13 16:42:01 -0300

    Curso5: iniciado curso. Alterado logica com listener para carregarCategorias pela logica com Saga.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { categoriasListener } from './middlewares/categorias';
+// import { categoriasListener } from './middlewares/categorias';
 import { itensListener } from './middlewares/itens';
 import buscaSlice from './reducers/busca'
 import carrinhoSlice from './reducers/carrinho';
 import categoriasSlice from './reducers/categorias';
 import itensSlice from './reducers/itens';
+import createSagaMiddleware from 'redux-saga';
+import { categoriasSaga } from './sagas/categorias';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
@@ -16,9 +20,12 @@ const store = configureStore({
   middleware:
     getDefaultMiddleware =>
       getDefaultMiddleware().prepend(
-        categoriasListener.middleware,
+        // categoriasListener.middleware,
         itensListener.middleware,
+        sagaMiddleware
       ),
 });
+
+sagaMiddleware.run(categoriasSaga);
 
 export default store;
---
+++ src/store/sagas/categorias.js
@@ -0,0 +1,44 @@
+import { call, delay, put, takeLatest } from 'redux-saga/effects';
+import { adicionarTodasAsCategorias, carregarCategorias } from 'store/reducers/categorias';
+import { createStandaloneToast } from '@chakra-ui/toast';
+import categoriasService from 'services/categorias';
+
+const { toast } = createStandaloneToast();
+
+function* observarCategorias() {  // worker
+  toast({
+    title: 'Carregando!',
+    description: 'Carregando categorias',
+    status: 'loading',
+    duration: 1000,
+    isClosable: true
+  });
+  try {
+    yield delay(1000);
+    const categorias = yield call(categoriasService.buscar);
+    yield put(adicionarTodasAsCategorias(categorias)); 
+    toast({
+      title: 'Sucesso!',
+      description: 'Categorias carregadas com sucesso',
+      status: 'success',
+      duration: 2000,
+      isClosable: true
+    });
+    
+  } catch (error) {
+    console.log('erro');
+    toast({
+      title: 'Erro!',
+      description: 'Erro na busca de categorias',
+      status: 'error',
+      duration: 2000,
+      isClosable: true
+    });
+  }
+}
+ 
+export function* categoriasSaga() {   // watcher
+  const tarefa = yield takeLatest(carregarCategorias, observarCategorias);
+  yield takeLatest(adicionarTodasAsCategorias, () => tarefa.cancel());
+  // yield takeLatest(adicionarTodasAsCategorias, function* () { yield cancel(tarefa); }); //outro jeito de cancelar a tarefa
+}
